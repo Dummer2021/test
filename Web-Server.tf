e73f8a2838ae207b80658e3bf0d604febbb3a58b
Dummer2021 <93815909+Dummer2021@users.noreply.github.com>
2021-12-24 10:43:40 +0500

    Update Web-Server.tf

--- Web-Server.tf
+++ Web-Server.tf
@@ -6,13 +6,22 @@
 # Copyleft ISA
 #----------------------------
 
-provider "aws" {
-  region                  = "eu-central-1"
-}
+ provider "aws" {
+ region                  = "eu-central-1"
+ }
 
-resource "aws_instance" "my_Web_Server" {
+ resource "aws_instance" "my_Web_Server" {
   ami                     = "ami-05d34d340fb1d89e5"  # Amazon Linux
   instance_type           = "t2.micro"
+  key_name                = "LinuxKey"
+
+  ebs_block_device {
+  device_name           = "/dev/xvda"
+  volume_size           = 10
+  volume_type           = "standard"
+  delete_on_termination = true
+  }
+
   vpc_security_group_ids  = [aws_security_group.my_web_server_sg.id]
 
   tags = {
@@ -21,21 +30,20 @@ resource "aws_instance" "my_Web_Server" {
 
   user_data = <<EOF
 #!/bin/bash
-yum -y update
-yum install -y epel-release
-yum install -y nginx
+sudo amazon-linux-extras install epel -y
+sudo yum install -y nginx
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>MyWebServer with IP: $myip</h2><br>Built by Terraform!" > /usr/share/nginx/html/index.html
-echo "<br><font color="green">Hello world ))" >> /usr/share/nginx/html/index.html
+sudo echo " <h2>WebServer with IP: $myip</h2><br>Built by Terraform! " > /usr/share/nginx/html/index.html
+sudo echo " <br><font color="green">Hello world ))! " >> /usr/share/nginx/html/index.html
 sudo systemctl start nginx
-chkconfig nginx on
+sudo chkconfig nginx on
 EOF
 
 }
 
   resource "aws_security_group" "my_web_server_sg" {
-    name                 = "WebServer-SG"
-    description          = "My-Web-SG"
+  name                 = "WebServer-SG"
+  description          = "My-Web-SG"
 
  ingress {
  description = "All"
@@ -43,13 +51,13 @@ EOF
  to_port     = 0
  protocol    = "-1"
  cidr_blocks = ["0.0.0.0/0"]
-}
+ }
 
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
+ egress {
+ from_port        = 0
+ to_port          = 0
+ protocol         = "-1"
+ cidr_blocks      = ["0.0.0.0/0"]
 
-    }
+ }
 }
